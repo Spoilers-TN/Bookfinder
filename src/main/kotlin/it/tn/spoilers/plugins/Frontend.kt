8318d37a11161cbcfc989d5e3572cdbe27032612
Francesco Masala <mail@francescomasala.me>
2022-08-04 12:50:03 +0200

    Fixed mustache templates

--- src/main/kotlin/it/tn/spoilers/plugins/Frontend.kt
+++ src/main/kotlin/it/tn/spoilers/plugins/Frontend.kt
@@ -5,16 +5,15 @@ import io.ktor.server.mustache.Mustache
 import io.ktor.server.mustache.MustacheContent
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
 import io.ktor.server.routing.*
 
-fun Application.configureTemplating() {
+fun Application.configureFrontend() {
     install(Mustache) {
-        mustacheFactory = DefaultMustacheFactory("templates/public")
+        mustacheFactory = DefaultMustacheFactory("templates")
     }
 
     routing {
-        get("/html-mustache") {
+        get("/") {
             call.respond(MustacheContent("index.hbs", mapOf("user" to MustacheUser(1, "user1"))))
         }
     }
