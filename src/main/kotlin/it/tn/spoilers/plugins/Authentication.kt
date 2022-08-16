7bb1b2ba173aef8a43f9dc6b54d74dc47608999f
Francesco Masala <mail@francescomasala.me>
2022-08-16 18:36:52 +0200

    Fixed google oauth and removed github oauth (pain)

--- src/main/kotlin/it/tn/spoilers/plugins/Authentication.kt
+++ src/main/kotlin/it/tn/spoilers/plugins/Authentication.kt
@@ -5,6 +5,7 @@ import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.apache.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.sessions.*
 import io.ktor.server.application.*
@@ -12,55 +13,44 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.*
 import java.time.Duration
-
+import kotlinx.serialization.json.*
 
 fun Application.configureAuthentication() {
     log.info("[!] Starting Plugin - Authentication.kt")
 
     data class UserSession(val token: String)
+    data class UserData(val id: String,
+                        val name: String,
+                        @SerialName("given_name") val givenName: String,
+                        @SerialName("family_name") val familyName: String,
+                        val picture: String,
+                        val locale: String)
     install(Sessions) {
-        cookie<UserSession>("user_session")
+        cookie<UserSession>("user_session") {
+            cookie.path = "/"
+            cookie.maxAgeInSeconds = Duration.ofDays(1).seconds
+        }
+        cookie<UserData>("user_data") {
+            cookie.path = "/profile"
+            cookie.maxAgeInSeconds = Duration.ofDays(1).seconds
+        }
     }
-
-    @Serializable
-    data class UserInfo(
-        val id: String,
-        val name: String,
-        @SerialName("given_name") val givenName: String,
-        @SerialName("family_name") val familyName: String,
-        val picture: String,
-        val locale: String
-    )
-    authentication {
+    install (Authentication) {
         oauth("auth-oauth-google") {
-            urlProvider = { "http://localhost:8080/google-callback" }
+            client = HttpClient(Apache)
+            urlProvider = { "https://bookfinder.spoilers.tn.it/google-callback" }
             providerLookup = {
                 OAuthServerSettings.OAuth2ServerSettings(
                     name = "google",
                     authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
-                    accessTokenUrl = "https://oauth2.googleapis.com/token",
+                    accessTokenUrl = "https://accounts.google.com/o/oauth2/token",
                     requestMethod = HttpMethod.Post,
                     clientId = "750408780393-hn4fhicn68f04o3f7d8o2ng097q2pgo6.apps.googleusercontent.com",
                     clientSecret = "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",
                     defaultScopes = listOf("https://www.googleapis.com/auth/userinfo.profile"),
                 )
             }
-            client = HttpClient(Apache)
-        }
-        oauth("auth-oauth-github") {
-            urlProvider = { "http://localhost:8080/github-callback" }
-            providerLookup = {
-                OAuthServerSettings.OAuth2ServerSettings(
-                    name = "github",
-                    authorizeUrl = "https://github.com/login/oauth/authorize",
-                    accessTokenUrl = "https://github.com/login/oauth/access_token",
-                    requestMethod = HttpMethod.Post,
-                    clientId = "2b14dd96e49e46f5de2b",
-                    clientSecret = "30b34c84a178729cc87d3985279c4b7be56266d3",
-                    defaultScopes = listOf("https://api.github.com/user"),
-                )
-            }
-            client = HttpClient(Apache)
+
         }
     }
     routing {
@@ -70,50 +60,46 @@ fun Application.configureAuthentication() {
             }
 
             get("/google-callback") {
-                val principal: OAuthAccessTokenResponse.OAuth2? = call.principal()
-                call.sessions.set(UserSession(principal?.accessToken.toString()))
-                call.respondRedirect("/google-hello")
-            }
-            get("/google-hello") {
-                val userSession: UserSession? = call.sessions.get()
-                if (userSession != null) {
-                    val userInfo: UserInfo = HttpClient(Apache).get("https://www.googleapis.com/oauth2/v2/userinfo") {
-                        headers {
-                            append(HttpHeaders.Authorization, "Bearer ${userSession.token}")
-                        }
-                    }.body()
-                    call.respondText("Hello, ${userInfo.name}!")
-                } else {
-                    call.respondText("Not authenticated")
-                }
-            }
-        }
-        authenticate("auth-oauth-github") {
-            get("github-login") {
-                // Redirects to 'authorizeUrl' automatically
-            }
-
-            get("/github-callback") {
-                val principal: OAuthAccessTokenResponse.OAuth2? = call.principal()
-                call.sessions.set(UserSession(principal?.accessToken.toString()))
-                call.respondRedirect("/github-hello")
-            }
-            get("/github-hello") {
-                val userSession: UserSession? = call.sessions.get()
-                if (userSession != null) {
-                    val userInfo: UserInfo = HttpClient(Apache).get("https://api.github.com/user") {
-                        headers {
-                            append(HttpHeaders.Authorization, "Bearer ${userSession.token}")
-                        }
-                    }.body()
-                    call.respondText("Hello, ${userInfo.name}!")
-                }
+                val principal = call.authentication.principal<OAuthAccessTokenResponse.OAuth2>()
+                    ?: error("No principal")
+                val json = HttpClient(Apache).get("https://www.googleapis.com/userinfo/v2/me") {
+                    header("Authorization", "Bearer ${principal.accessToken}")
+                }.bodyAsText()
+                val UserDataFromJson = Json.decodeFromString<UserInfo>(json)
+                call.sessions.set(UserSession(principal.accessToken))
+                println(UserDataFromJson.toString())
+                call.sessions.set(UserData(
+                    UserDataFromJson.id,
+                    UserDataFromJson.name,
+                    UserDataFromJson.givenName,
+                    UserDataFromJson.familyName,
+                    UserDataFromJson.picture,
+                    UserDataFromJson.locale
+                ))
+                call.respondRedirect("/profile")
             }
         }
-        authenticate("auth-oauth-google") {
+        get("/profile") {
+            val UserSession = call.sessions.get<UserSession>()
+            val UserData = call.sessions.get<UserData>()
+            if(UserSession != null) {
+                println(UserData?.familyName)
+                call.respondText("Benvenuto ${UserData?.name}, il tuo ID è ${UserData?.id} ")
+            } else {
+                call.respond(HttpStatusCode.Unauthorized, "Not authenticated")
+            }
 
         }
     }
     log.info("[✓] Started Plugin - Authentication.kt")
 
 }
+@Serializable
+data class UserInfo(
+    val id: String,
+    val name: String,
+    @SerialName("given_name") val givenName: String,
+    @SerialName("family_name") val familyName: String,
+    val picture: String,
+    val locale: String
+)
