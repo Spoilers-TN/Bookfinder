c54652266460ac1fc31684a5b13304e75f3a7c29
Francesco Masala <mail@francescomasala.me>
2022-08-18 17:25:33 +0200

    Every time the application returns an error this includes a message from whatthecommit.com

--- src/main/kotlin/it/tn/spoilers/plugins/Errors.kt
+++ src/main/kotlin/it/tn/spoilers/plugins/Errors.kt
@@ -1,5 +1,9 @@
 package it.tn.spoilers.plugins
 
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.mustache.*
@@ -7,62 +11,75 @@ import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import kotlinx.serialization.Serializable
 
+suspend fun ReturnError(): String {
+    return HttpClient(Apache).get("http://whatthecommit.com/index.txt") {
+    }.bodyAsText()
+}
+
 fun Application.configureErrors() {
     log.info("[!] Starting Plugin - Errors.kt")
     install(StatusPages) {
         status(HttpStatusCode.NotFound) { call, status ->
             call.respond(status= status, MustacheContent("error.hbs", mapOf("error" to Error(
                 status.value.toString(),
-                status.description
+                status.description,
+                ReturnError()
             )
             )))
         }
         status(HttpStatusCode.Forbidden) { call, status ->
             call.respond(status= status, MustacheContent("error.hbs", mapOf("error" to Error(
                 status.value.toString(),
-                status.description
+                status.description,
+                ReturnError()
             )
             )))
         }
         status(HttpStatusCode.Unauthorized) { call, status ->
             call.respond(status= status, MustacheContent("error.hbs", mapOf("error" to Error(
                 status.value.toString(),
-                status.description
+                status.description,
+                ReturnError()
             )
             )))
         }
         status(HttpStatusCode.InternalServerError) { call, status ->
             call.respond(status= status, MustacheContent("error.hbs", mapOf("error" to Error(
                 status.value.toString(),
-                status.description
+                status.description,
+                ReturnError()
             )
             )))
         }
         status(HttpStatusCode.BadGateway) { call, status ->
             call.respond(status= status, MustacheContent("error.hbs", mapOf("error" to Error(
                 status.value.toString(),
-                status.description
+                status.description,
+                ReturnError()
             )
             )))
         }
         status(HttpStatusCode.ServiceUnavailable) { call, status ->
             call.respond(status= status, MustacheContent("error.hbs", mapOf("error" to Error(
                 status.value.toString(),
-                status.description
+                status.description,
+                ReturnError()
             )
             )))
         }
         status(HttpStatusCode.GatewayTimeout) { call, status ->
             call.respond(status= status, MustacheContent("error.hbs", mapOf("error" to Error(
                 status.value.toString(),
-                status.description
+                status.description,
+                ReturnError()
             )
             )))
         }
         status(HttpStatusCode.NotImplemented) {call, status ->
             call.respond(status= status, MustacheContent("error.hbs", mapOf("error" to Error(
                 status.value.toString(),
-                status.description
+                status.description,
+                ReturnError()
             )
             )))
         }
@@ -70,13 +87,15 @@ fun Application.configureErrors() {
             if (cause is AuthorizationException) {
                 call.respond(status= HttpStatusCode.Forbidden, MustacheContent("error.hbs", mapOf("error" to Error(
                     HttpStatusCode.Forbidden.value.toString(),
-                    cause.toString()
+                    cause.toString(),
+                    ReturnError()
                 )
                 )))
             } else {
                 call.respond(status= HttpStatusCode.InternalServerError, MustacheContent("error.hbs", mapOf("error" to Error(
                     HttpStatusCode.InternalServerError.value.toString(),
-                    cause.toString()
+                    cause.toString(),
+                    ReturnError()
                 )
                 )))
             }
@@ -86,6 +105,6 @@ fun Application.configureErrors() {
 }
 
 @Serializable
-data class Error(val code: String, val descr: String)
+data class Error(val code: String, val descr: String, val meme: String)
 class AuthenticationException : RuntimeException()
 class AuthorizationException : RuntimeException()
