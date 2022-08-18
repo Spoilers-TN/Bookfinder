9b79978b4979b7f3402fbbcef0e3d00aac2d91d1
Francesco Masala <mail@francescomasala.me>
2022-08-18 14:37:16 +0200

    Improved error handling

--- src/main/kotlin/it/tn/spoilers/plugins/Errors.kt
+++ src/main/kotlin/it/tn/spoilers/plugins/Errors.kt
@@ -2,40 +2,83 @@ package it.tn.spoilers.plugins
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.mustache.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
+import kotlinx.serialization.Serializable
 
 fun Application.configureErrors() {
     log.info("[!] Starting Plugin - Errors.kt")
     install(StatusPages) {
         status(HttpStatusCode.NotFound) { call, status ->
-            call.respondText(text = "404: Not Found", status = HttpStatusCode.NotFound)
+            call.respond(status= status, MustacheContent("error.hbs", mapOf("error" to Error(
+                status.value.toString(),
+                status.description
+            )
+            )))
         }
         status(HttpStatusCode.Forbidden) { call, status ->
-            call.respondText(text = "403: Forbidden", status = HttpStatusCode.Forbidden)
+            call.respond(status= status, MustacheContent("error.hbs", mapOf("error" to Error(
+                status.value.toString(),
+                status.description
+            )
+            )))
         }
         status(HttpStatusCode.InternalServerError) { call, status ->
-            call.respondText(text = "500: Internal Server Error", status = HttpStatusCode.InternalServerError)
+            call.respond(status= status, MustacheContent("error.hbs", mapOf("error" to Error(
+                status.value.toString(),
+                status.description
+            )
+            )))
         }
         status(HttpStatusCode.BadGateway) { call, status ->
-            call.respondText(text = "502: Bad Gateway", status = HttpStatusCode.BadGateway)
+            call.respond(status= status, MustacheContent("error.hbs", mapOf("error" to Error(
+                status.value.toString(),
+                status.description
+            )
+            )))
         }
         status(HttpStatusCode.ServiceUnavailable) { call, status ->
-            call.respondText(text = "503: Service Unavailable", status = HttpStatusCode.ServiceUnavailable)
+            call.respond(status= status, MustacheContent("error.hbs", mapOf("error" to Error(
+                status.value.toString(),
+                status.description
+            )
+            )))
         }
         status(HttpStatusCode.GatewayTimeout) { call, status ->
-            call.respondText(text = "504: Gateway Timeout", status = HttpStatusCode.GatewayTimeout)
+            call.respond(status= status, MustacheContent("error.hbs", mapOf("error" to Error(
+                status.value.toString(),
+                status.description
+            )
+            )))
+        }
+        status(HttpStatusCode.NotImplemented) {call, status ->
+            call.respond(status= status, MustacheContent("error.hbs", mapOf("error" to Error(
+                status.value.toString(),
+                status.description
+            )
+            )))
         }
         exception<Throwable> { call, cause ->
             if (cause is AuthorizationException) {
-                call.respondText(text = "403: $cause", status = HttpStatusCode.Forbidden)
+                call.respond(status= HttpStatusCode.Forbidden, MustacheContent("error.hbs", mapOf("error" to Error(
+                    HttpStatusCode.Forbidden.value.toString(),
+                    cause.toString()
+                )
+                )))
             } else {
-                call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
+                call.respond(status= HttpStatusCode.InternalServerError, MustacheContent("error.hbs", mapOf("error" to Error(
+                    HttpStatusCode.InternalServerError.value.toString(),
+                    cause.toString()
+                )
+                )))
             }
         }
     }
     log.info("[✓] Started Plugin - Errors.kt")
 }
 
+@Serializable
+data class Error(val code: String, val descr: String)
 class AuthenticationException : RuntimeException()
 class AuthorizationException : RuntimeException()
