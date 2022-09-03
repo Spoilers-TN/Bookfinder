35c7bd9599cd2b33ebebf4c77a5dc40b29542d89
Francesco Masala <mail@francescomasala.me>
2022-09-03 07:49:49 +0200

    Added the AnnouncementsService class that helps me with the DB (GM Everyone!)

--- src/main/kotlin/it/tn/spoilers/database/services/AnnouncementsService.kt
+++ src/main/kotlin/it/tn/spoilers/database/services/AnnouncementsService.kt
@@ -1,2 +1,37 @@
 package it.tn.spoilers.database.services
 
+
+import it.tn.spoilers.database.models.Announcements
+import org.litote.kmongo.*
+
+class AnnouncementsService {
+    private val client = KMongo.createClient("")
+    private val database = client.getDatabase("bookfinder")
+    private val announcementsCollection = database.getCollection<Announcements>("Announcements")
+
+    fun create(user: Announcements): Id<Announcements>?  {
+        announcementsCollection.insertOne(user)
+        return user.id
+    }
+
+    fun findAll(): List<Announcements> =
+        announcementsCollection.find().toList()
+
+    fun findByID(id: String): List<Announcements> {
+        val caseSensitiveTypeSafeFilter = Announcements::Announcement_ID eq id
+        return announcementsCollection.find(caseSensitiveTypeSafeFilter)
+            .toList()
+    }
+
+    fun findByISBN(isbn: Long): List<Announcements> {
+        val caseSensitiveTypeSafeFilter = Announcements::Announcement_Book eq isbn
+        return announcementsCollection.find(caseSensitiveTypeSafeFilter)
+            .toList()
+    }
+
+    fun findByUser(UserID: String): List<Announcements> {
+        val caseSensitiveTypeSafeFilter = Announcements::Announcement_User eq UserID
+        return announcementsCollection.find(caseSensitiveTypeSafeFilter)
+            .toList()
+    }
+}
