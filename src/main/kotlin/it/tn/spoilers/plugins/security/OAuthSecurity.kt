76634e5af393c1cb6d9a95feae31c0fabf9e9497
Francesco Masala <mail@francescomasala.me>
2022-09-09 21:59:30 +0200

    Migrated to pebble templating engine

--- src/main/kotlin/it/tn/spoilers/plugins/security/OAuthSecurity.kt
+++ src/main/kotlin/it/tn/spoilers/plugins/security/OAuthSecurity.kt
@@ -7,7 +7,6 @@ import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
-import io.ktor.server.mustache.*
 import io.ktor.server.pebble.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
