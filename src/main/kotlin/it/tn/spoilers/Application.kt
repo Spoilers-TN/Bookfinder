411132cef3656b82e9451b8be51a302d6110df5c
Riccardo Furlan <riccardo.furlan@buonarroti.tn.it>
2023-04-17 15:06:25 +0200

    eliminati service api non utilizzate
    Co-Authored-By: Giacomo Berti <bertii.giacomo@gmail.com>

--- src/main/kotlin/it/tn/spoilers/Application.kt
+++ src/main/kotlin/it/tn/spoilers/Application.kt
@@ -6,7 +6,6 @@ import io.ktor.server.netty.*
 import it.tn.spoilers.extras.DisableLogging
 import it.tn.spoilers.extras.DisableMongoLogging
 import it.tn.spoilers.extras.EnableSentry
-import it.tn.spoilers.plugins.api.configureSearchApi
 import it.tn.spoilers.plugins.api.configureBooksApi
 import it.tn.spoilers.plugins.api.configureReviewsApi
 import it.tn.spoilers.plugins.api.configureSchoolsApi
@@ -59,7 +58,6 @@ fun Application.module() {
     configureCompression()
     configureCORS()
     configureBooksApi()
-    configureSearchApi()
     configureSchoolsApi()
     configureUsersApi()
     configureReviewsApi()
